9137a2bdbfd141e76e62eea1ef6c354565204b3e
Shwetha <Shwetha@192.168.1.96>
2021-01-08 15:42:52 +0530

    First commit

---
+++ ActiTime/src/test/java/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+@Test
+public void testCreateCustomer() {
+	Reporter.log("CreateCustomer",true);
+}
+}
